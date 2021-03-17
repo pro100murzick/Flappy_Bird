88ffa041edfe914d4d3a37757239ad56fd38e45e
unknown <mkiricenko289@gmail.com>
2021-03-17 17:30:17 +0200

    Add level object and level script. Add onclick() function to button

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LaodMainMenu()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void nApplicationQuit()
+    {
+        Application.Quit();
+    }
+}
