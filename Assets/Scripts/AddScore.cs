8028da130cdeb629edb7386bd1192162a909a99d
unknown <mkiricenko289@gmail.com>
2021-03-24 16:43:58 +0200

    Add sound effects

--- Assets/Scripts/AddScore.cs
+++ Assets/Scripts/AddScore.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class AddScore : MonoBehaviour
 {
-
+    [SerializeField] AudioClip addScoreSound;
+    [SerializeField] [Range(0, 1)] float addScoreSoundVolume;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        AudioSource.PlayClipAtPoint(addScoreSound, Camera.main.transform.position, addScoreSoundVolume);
         FindObjectOfType<Score>().AddScore();
     }
 
